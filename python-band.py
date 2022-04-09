87aa46f9511b8acc40b74dadc08ec39862799ca2
kdh8219 <65698239+kdh8219@users.noreply.github.com>
2022-04-09 16:35:52 +0900

    day 21

--- python-band.py
+++ python-band.py
@@ -116,22 +116,124 @@ def day13():
     startTime = t.time()  # 시작시간
     start = 10000000
     end = 20000000
+    if start > end:
+        start, end = end, start
     print((end+start)*(((end+1)-start)/2))
     endTime = t.time()
     eTime = endTime-startTime  # 경과시간(걸린 시간)
     print('경과시간:%.3f초' % eTime)
 
 
-# def day14():
-#     while True:
-#         _input = int(input("정수 n:"))
-#         if _input == 0:
-#             break
-#         sum = 0
-#         for i in range(1, _input+1):
-#             if i//2 == 1:
-#                 sum += i
-#         print(sum)
+def day13_function(start, end):
+    if start > end:
+        start, end = end, start
+    return((end+start)*(((end+1)-start)/2))
 
 
-day12()
+def day14():
+    while True:
+        _input = int(input("정수 n:"))
+        if _input == 0:
+            break
+        sum = 0
+        for i in range(1, _input+1):
+            if i % 2 == 1:
+                sum += i
+        print(sum)
+
+
+def day15():
+    while True:
+        _input = int(input("층수 n:"))
+        nm = 1
+        if _input == 0:
+            break
+        for i in range(0, _input):
+            for ind2, vel2 in enumerate(range(0, _input)):
+                if ind2+1 == _input:
+                    print(str(nm)[-1])
+                else:
+                    print(str(nm)[-1], end="")
+                nm += 1
+
+
+def day16():
+    while True:
+        _input = int(input("정수 n:"))
+        if _input == 0:
+            break
+        odd = 0
+        even = 0
+        all = 0
+        for i in range(1, _input+1):
+            if i % 2 == 1:
+                odd += i
+            if i % 2 == 0:
+                even += i
+            all += i
+        print(f"홀수 합계:{odd}\n짝수합계:{even}\n총 합계:{all}")
+
+
+def day17():
+    while True:
+        _input = int(input("층수 n:"))
+        nm = 1
+        if _input == 0:
+            break
+        for ind1, vel1 in enumerate(range(0, _input)):
+            for ind2, vel2 in enumerate(range(0, _input)):
+                if ind2 == ind1:
+                    print(str(nm)[-1])
+                    nm += 1
+                    break
+                else:
+                    print(str(nm)[-1], end="")
+                    nm += 1
+
+
+def day18():
+    while True:
+        _input = int(input("정수:"))
+        if _input == 0:
+            break
+        list = []
+        for i in range(1, _input+1):
+            if _input % i == 0:
+                list.append(i)
+        print(str(list).replace('[', '').replace(']', ''))
+
+
+def day19():
+    while True:
+        try:
+            a, b, c = map(int, input("정수3개 띄어쓰기로 입력: ").split())
+        except:
+            break
+        if (a < b and a > c) or (a > b and a < c):
+            print(a)
+        elif (b < a and b > c) or (b > a and b < c):
+            print(b)
+        elif (c < a and c > b) or (c > a and c < b):
+            print(c)
+
+
+def day20():
+    wordList = ['cat', 'dog', 'pig', 'hen', 'cow', 'duck', 'cat']
+    while True:
+        _input = input("단어입력:")
+        if _input == "\n":
+            break
+        if _input in wordList:
+            print(f"{_input}은 {wordList.index(_input)+1}번째에 있다.")
+
+
+def day21():
+    wordList = ['cat', 'dog', 'pig', 'hen', 'cow', 'duck', 'cat']
+    while True:
+        _input = input("단어입력:")
+        if _input == "\n":
+            break
+        if _input in wordList:
+            print(f"{_input}은 {wordList.index(_input)+1}번째에 있다.")
+        else:
+            print(f"{_input}은 없음.")
